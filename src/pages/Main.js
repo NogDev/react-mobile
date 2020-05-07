a55ba9b4221bb8b96d9d1a734744bf5229dbccc1
nog <nogueira@pm.ba.gov.br>
2020-05-07 00:05:10 -0300

    exibindo foto de dev no mapa

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps'
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
 
 
@@ -34,13 +34,28 @@ function Main(){
     if(!currentRegion){
         return null;
     }
-    return <MapView  initialRegion={currentRegion} style= {style.map} />
+    return (        
+        <MapView  initialRegion={currentRegion} style= {style.map}>
+            <Marker coordinate={{latitude: -12.9158725, longitude: -38.4369632}}>
+                <Image style={style.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/25827712?s=400&u=4b4df62325f2118daa9a30a9f7c4702db4d10606&v=4'}}/>
+            </Marker> 
+        </MapView>
+    );
 }
 
 const style = StyleSheet.create({
     map: {
-        flex: 1
+        flex: 1 
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
     }
+
 })
 
 
