0e731c4704c1b5fd7508b4553d5f604ead916795
nog <nogueira@pm.ba.gov.br>
2020-05-07 00:14:10 -0300

    adicionado informações ao clicar na imagem

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps'
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
 
 
@@ -38,6 +38,14 @@ function Main(){
         <MapView  initialRegion={currentRegion} style= {style.map}>
             <Marker coordinate={{latitude: -12.9158725, longitude: -38.4369632}}>
                 <Image style={style.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/25827712?s=400&u=4b4df62325f2118daa9a30a9f7c4702db4d10606&v=4'}}/>
+
+                <Callout>
+                    <View style={style.callout}>
+                        <Text style={style.devName}>NogDev</Text>
+                        <Text style={style.devBio}> Boioo phogougougoou</Text>
+                        <Text style={style.devTechs}>Java e Joomla</Text>
+                    </View>
+                </Callout>
             </Marker> 
         </MapView>
     );
@@ -54,6 +62,24 @@ const style = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 4,
         borderColor: '#FFF'
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color:'#676',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
     }
 
 })
