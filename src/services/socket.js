8b34e7a517d5ec2c7c2b36e19c7c32cf6cdab5b1
nog <nogueira@pm.ba.gov.br>
2020-05-10 11:15:42 -0300

    atualizando pesquisa via socket

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,6 +4,11 @@ const socket = socketio('http://192.168.0.111:3333', {
     autoConnect: false,
 });
 
+function subscribeToNewDevs(subcribeFunction) {
+    socket.on('new-dev', subcribeFunction);
+  }
+  
+
 function connect(latitude, longitude, techs){
     
     socket.io.opts.query = {
@@ -13,10 +18,6 @@ function connect(latitude, longitude, techs){
     };
     
     socket.connect();
-
-    socket.on('message', text => {
-        console.log(text);
-    })
 }
 
 function disconnect(){
@@ -28,4 +29,5 @@ function disconnect(){
 export {
     connect,
     disconnect,
+    subscribeToNewDevs
 };
